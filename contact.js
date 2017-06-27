e23353e4e64054d8463a4ff973092f7cf724ace9
Gableb <gableb@hotmail.fr>
2017-06-27 15:57:39 +0200

    Commit

--- contact.js
+++ contact.js
@@ -1,41 +1,31 @@
 $(document).ready(function(){
 
-  var $nom = $('#lname'),
-        $prenom = $('#fname'),
-        $phone = $('#phone'),
+    var $prenom = $('#fname'),
+        $nom = $('#lname'),
         $mail = $('#mail'),
-        $objet = $('#objet'),
-        $valider=$('#valider')
-        $message = $('#message');
-
-  $valider.click(function(e){
-    e.preventDefault(); // on annule la fonction par défaut du bouton d'envoi
-
-    // puis on lance la fonction de vérification sur tous les champs :
-    verifier($prenom);
-    verifier($nom);
-    verifier($message);
-    isEmail($mail)
+        $phone = $('#phone'),
+        $envoi = $('#envoi'),
+        $message = $('#message'),
+        $champ = $('.champ');
 
 
-  });
-});
+    $envoi.click(function(e){
+        e.preventDefault(); // on annule la fonction par défaut du bouton d'envoi
 
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if(regex){
-        mail.css({
-    	    borderColor : 'red',
-    	    color : 'red'
-    	});
-  return regex.test(email);
-}
+        // puis on lance la fonction de vérification sur tous les champs :
+        verifier($prenom);
+        verifier($nom);
+        verifier($mail);
+        verifier($message);
+    });
 
-function verifier(champ){
-    if(champ.val() == ""){ // si le champ est vide
-        champ.css({ // on rend le champ rouge
-    	    borderColor : 'red',
-    	    color : 'red';
+    function verifier(champ){
+        if(champ.val() == ""){ // si le champ est vide
+            champ.css({ // on rend le champ rouge
+    	        borderColor : 'red',
+    	        color : 'red'
+    	    });
+        }
     }
-  }
-}
+
+});
--- recherche.php
+++ recherche.php
@@ -3,15 +3,53 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<title>Recherche</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="Geany 1.27" />
+  	<link href="style.css" rel="stylesheet" type="text/css"/>
+ 		<link href="recherche.css" rel="stylesheet" type="text/css"/>
+  	<link rel="icon" type="image/png" href="favicon.png"/>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="generator" content="Geany 1.27" />
+  	<title>Recherche</title>
 </head>
 
 <body>
-
+	<!-- DEBUT HEADER -->
+	<div id="header">
+		<div id="navbar">
+			<ul>
+				<li><a href="accueil.html">Accueil</a></li>
+				<li><a href="comparatif.html">Nos offres</a></li>
+				<li class="dropdown">
+					<a href="formations.html" class="dropbtn">Nos formations</a>
+					<div class="dropdown-content">
+						<a href="formations/formation-1.html">Formation 1</a>
+						<a href="formations/formation-2.html">Formation 2</a>
+						<a href="formations/formation-3.html">Formation 3</a>
+					</div>
+				</li>
+				<li class="dropdown">
+					<a href="entreprise.html" class="dropbtn">L'entreprise</a>
+					<div class="dropdown-content">
+						<a href="entreprise/equipe.html">L'équipe</a>
+					</div>
+				</li>
+				<li><a href="contact.html">Nous contacter</a></li>
+				<li><a href="recherche.php">Recherche</a></li>
+				<li>
+					<ul id="networks">
+						<li><a href="http://www.twitter.com"><img src="icons/twitter.png" alt="twitter" class="icon"></a></li>
+						<li><a href="http://www.facebook.com"><img src="icons/facebook.png" alt="facebook" class="icon"></a></li>
+						<li><a href="http://www.linkedin.com"><img src="icons/linkedin.png" alt="linkedin" class="icon"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div id="banner">
+			<img src="banner.jpg" alt="banner">
+		</div>
+	</div>
+	<!-- FIN HEADER -->
 	<form action="recherche.php" method="post">
-		<!-- Ici va s'afficher le terme saisi dans le champ recherche
+		<!-- Ici va s'afficher le terme saisie dans le champ recherche
 				si il n'est pas nul -->
 		<?php if($_POST['recherche'] != ''): ?>
 			<input type="text" name="recherche" value="<?php echo $_POST['recherche'] ; ?>">
@@ -29,23 +67,49 @@
 <!-- Ici va s'afficher la liste des resultats pour le mot cle "html"
 	A adapter selon vos propres affichage de resultats -->
 
-<!--  tableau de reponse à mettre en forme 	-->
-
-<!-- titre				theme	duree	format	type accès 	lien	-->
-<!--
-	initiation			html	2h	en ligne	free		lien1
-	formation de base	html	2j	sur site	pro			lien2
-	formation avancée	html	2j	sur site	pro			lien3
-	formation expert	html	3j	sur site	premium		lien4
--->
-
-resultats
+<table>
+	<tr>
+		<td>Nom de la formation</td>
+		<td>Matière</td>
+		<td>Durée</td>
+		<td>Offre correspondante</td>
+	</tr>
+	<tr>
+		<td>Initiation</td>
+		<td>HTML</td>
+		<td>2h</td>
+		<td>FREE</td>
+		<td><a href="#">Consulter cette formation</a></td>
+	</tr>
+	<tr>
+		<td>Formation de base</td>
+		<td>HTML</td>
+		<td>16h</td>
+		<td>PRO</td>
+		<td><a href="#">Consulter cette formation</a></td>
+	</tr>
+	<tr>
+		<td>Formation avancée</td>
+		<td>HTML</td>
+		<td>16h</td>
+		<td>PRO</td>
+		<td><a href="#">Consulter cette formation</a></td>
+	</tr>
+	<tr>
+		<td>Formation expert</td>
+		<td>HTML</td>
+		<td>24h</td>
+		<td>PREMIUM</td>
+		<td><a href="#">Consulter cette formation</a></td>
+	</tr>
+</table>
 
 <?php else: ?>
 <!-- Ici va s'afficher la liste des resultats pour les autres recherche
 	=> il faut adapter le message pour pas de résultats  -->
-
-pas de résultats
+<table>
+	<td>Aucun résultat</td>
+</table>
 
 <?php endif; ?>
 
