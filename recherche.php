de7f890c03ca5250058a7449beb9d221b23c869a
Gableb <gableb@hotmail.fr>
2017-06-27 11:54:36 +0200

    Commit

--- recherche.php
+++ recherche.php
@@ -3,32 +3,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<title>sans titre</title>
+	<title>Recherche</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="generator" content="Geany 1.27" />
 </head>
 
 <body>
-	
+
 	<form action="recherche.php" method="post">
-		<!-- Ici va s'afficher le terme saisie dans le champ recherche  
+		<!-- Ici va s'afficher le terme saisi dans le champ recherche
 				si il n'est pas nul -->
 		<?php if($_POST['recherche'] != ''): ?>
 			<input type="text" name="recherche" value="<?php echo $_POST['recherche'] ; ?>">
 		<?php else: ?>
 			<input type="text" name="recherche" >
 		<?php endif; ?>
-		
+
 		<input type="submit" value="Submit">
-		
+
 	</form>
-	
+
 
 
 <?php if(strtolower($_POST['recherche']) == 'html'): ?>
-<!-- Ici va s'afficher la liste des resultats pour le mot cle "html"  
+<!-- Ici va s'afficher la liste des resultats pour le mot cle "html"
 	A adapter selon vos propres affichage de resultats -->
-	
+
 <!--  tableau de reponse à mettre en forme 	-->
 
 <!-- titre				theme	duree	format	type accès 	lien	-->
@@ -42,13 +42,13 @@
 resultats
 
 <?php else: ?>
-<!-- Ici va s'afficher la liste des resultats pour les autres recherche 
+<!-- Ici va s'afficher la liste des resultats pour les autres recherche
 	=> il faut adapter le message pour pas de résultats  -->
 
 pas de résultats
 
 <?php endif; ?>
-	
+
 </body>
 
 </html>
